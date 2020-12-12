11003dca068d17d197505d5a2a7e86876fd567a6
Sabique1234 <66312211+Sabique1234@users.noreply.github.com>
2020-12-12 15:45:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import * as React from 'react';
+
+import { StyleSheet, Text, View } from 'react-native';
+
+import ScanScreen from './screens/ScanScreen';
+
+
+export default class App extends React.Component {
+
+  render() 
+  {
+       return (
+      <ScanScreen/>
+    );
+  }
+}
+
+const styles=StyleSheet.create({
+  //
+})
